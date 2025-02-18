1b8555d8f9825758f0349e8b3d922fbc424fa990
moumniyoussef <youssef18moumni@gmail.com>
2025-02-18 18:49:05 +0100

    done

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ NAME = so_long
 
 CC = cc
 
-CFLAGS = -Wall -Wextra -Werror
+CFLAGS = -Wall -Wextra -Werror -fsanitize=address
 
 MFLAG = -lmlx -framework OpenGL -framework AppKit
 
@@ -27,4 +27,4 @@ clean :
 fclean : clean
 	@rm -f $(NAME)
 
-re : clean all
+re : fclean all
--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: youmoumn <youmoumn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 11:14:04 by youmoumn          #+#    #+#             */
-/*   Updated: 2025/02/17 12:26:22 by youmoumn         ###   ########.fr       */
+/*   Updated: 2025/02/18 16:51:33 by youmoumn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,13 @@ int main(int ac, char **av)
 	(void)av;
 	void	*img ;
 	void	*mlx;
+	void	*image;
 
 	mlx = mlx_init();
 	img = mlx_new_window(mlx, 1200, 1000, "so_long");
+	image = mlx_new_image(mlx, 1200, 100);
 	int i = 0;
-	while(i < 1200)
+	while(i <= 1200)
 	{
 		mlx_pixel_put(mlx, img, 100, 100, R);
 		i++;
